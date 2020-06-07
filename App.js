45eeeab6ebf5a4a994a5464a4c660b92711fc91d
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-06-07 15:45:46 -0300

    Primeiro commit.

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Simples from './src/componentes/Simples';
+import ParImpar from './src/componentes/ParImpar';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Simples texto="Flexível" />
+      <ParImpar numero={33} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default App;
---
+++ src/componentes/ParImpar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Padrao from '../estilos/Padrao';
+
+export default function (props) {
+
+    return (
+        <View>
+            {
+                props.numero % 2 === 0
+                    ?
+                    <Text style={[Padrao.ex]}>Par</Text>
+                    :
+                    <Text style={[Padrao.ex]}>Impar</Text>
+            }
+        </View>
+    )
+}
